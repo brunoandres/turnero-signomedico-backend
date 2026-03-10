c3ca2edea5f058a80f590829c6e719f2f644c519
Bruno <brunoandres2013@gmail.com>
2026-03-10 20:22:05 -0300

    ajuste websocket

--- src/gateways/turnos.gateway.ts
+++ src/gateways/turnos.gateway.ts
@@ -1,49 +1,51 @@
 import { WebSocketGateway, WebSocketServer, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { SubscribeMessage } from '@nestjs/websockets';
-import { TurnosService } from '../turnos/turnos.service';
-import { Inject } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Turno } from '../turnos/schemas/turnos.schema';
-import { Model } from 'mongoose';
 
 @WebSocketGateway({
   cors: {
-    origin: ['https://turnero-cipbyte.vercel.app'],
-    credentials: true,
+    origin: ['https://turnero-cipbyte.vercel.app']
   },
 })
-export class TurnosGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class TurnosGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
-  @WebSocketServer()
-  server: Server;
+  //constructor(private turnoService: TurnosService) {}
 
-  async handleConnection(client: Socket) {
+  @WebSocketServer() server: Server;
+
+  async handleConnection(client: any, ...args: any[]) {
+    /*const count = await this.turnoService.countDocuments();
+    if (count === 0) {
+      this.server.emit('database-empty', { message: 'The database is currently empty' });
+    }*/
     console.log(`Cliente conectado: ${client.id}`);
   }
 
-  handleDisconnect(client: Socket) {
+  handleDisconnect(client: any) {
     console.log(`Cliente desconectado: ${client.id}`);
   }
 
   afterInit(server: Server) {
-    console.log('Servidor WebSocket iniciado');
+    console.log('Servidor WebSocket iniciado:');
   }
 
   enviarActualizacion(turnos: any) {
-    this.server.emit('actualizacion-turnos', turnos);
+    //console.log('recibiendo actualización: ', turnos)
+    this.server.to('actualizacion-turnos');
   }
 
-  @SubscribeMessage('event_message')
-  handleIncommingMessage(client: Socket, payload: any) {
-    this.server.emit('new_message', payload);
+  @SubscribeMessage('event_message') //TODO Backend
+  handleIncommingMessage(
+    client: Socket,
+    id: { id: string },
+  ) {
+    this.server.to('Enviando actualización desde el backend').emit('new_message');
   }
 
   @SubscribeMessage('catch-error')
   handleCatchError(client: Socket, payload: any): void {
     try {
-      // lógica
+      // Hacer algo aquí que pueda generar un error
     } catch (error) {
       client.emit('error', { message: 'Ocurrió un error en el servidor' });
     }
